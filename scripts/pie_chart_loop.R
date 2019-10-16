e2064f5bd04a5298559f05a733e0e75152aa8de2
Andreas Duenser <andreas.duenser@csiro.au>
2019-10-16 17:03:42 +1100

    new script to produce pie charts for all councils

---
+++ scripts/pie_chart_loop.R
@@ -0,0 +1,29 @@
+library(cowplot)
+
+# create graphing function
+council.graph <- function(df, na.rm = TRUE, ...){
+  
+  # create list of counties in data to loop over 
+  council_list <- unique(df$council)
+  
+  # create for loop to produce ggplot2 graphs 
+  for (i in seq_along(council_list)) { 
+    
+    # create plot for each county in df 
+    plot <- 
+      plot_ly(subset(df, df$council==council_list[i]), labels = ~sentiment, values = ~freq, type = 'pie') %>%
+      layout(title = council_list[i],
+             xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+    
+ #  print(plot)
+  paste0("plot_",council_list[i]) <- plot
+  }
+}
+
+council.graph(council_sentiment)
+             
+             
+             
+  
+  
